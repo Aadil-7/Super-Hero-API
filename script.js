b128bacec378ff97cc7ce88b89b1755dc7fb9463
Aadil-7 <aadilbhusnur007@gmail.com>
2022-08-10 20:15:45 +0530

    added search function

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // const baseurl= `https://superheroapi.com/api.php/110151828463508/id`
 
 const button = document.getElementById(`newherobutton`)
-const getRandomHero = (id) =>{
-fetch(`https://superheroapi.com/api.php/110151828463508/${id}`)
-.then(Response => Response.json())
-.then(json =>{ 
-const img = document.getElementById('heroimage');
-img.setAttribute('src', `${json.image.url}`)
-const name = document.getElementById(`heroname`)
-name.innerText = json.name
-})
+const getRandomHero = (id) => {
+    fetch(`https://superheroapi.com/api.php/110151828463508/${id}`)
+        .then(Response => Response.json())
+        .then(json => {
+            const img = document.getElementById('heroimage');
+            img.setAttribute('src', `${json.image.url}`)
+            const name = document.getElementById(`heroname`)
+            name.innerText = json.name
+        })
 }
 
-button.onclick = () =>{getRandomHero(
-    Math.ceil(Math.random()* 731)
-)
+const searchButton = document.getElementById('searchButton')
+const searchBar = document.getElementById('searchbar')
+const searchHero = (search) => {
+    fetch(`https://superheroapi.com/api.php/110151828463508/search/${search}`)
+        .then(Response => Response.json())
+        .then(json => {
+            const hero = json.results[0]
+            const img = document.getElementById('heroimage');
+            img.setAttribute('src', `${hero.image.url}`)
+            const name = document.getElementById(`heroname`)
+            name.innerText = hero.name
+        })
+}
+
+button.onclick = () => {
+    getRandomHero(
+        Math.ceil(Math.random() * 731)
+    )
+}
+
+searchButton.onclick = () => {
+    searchHero(searchBar.value);
 }
